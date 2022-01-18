534675fdbf959b7488f6eda1a03a5b182fb06e99
Bolotov Alexander <distalker1@gmail.com>
2022-01-18 08:42:56 +0300

    initial

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,13 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld otherBean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat catBean = applicationContext.getBean(Cat.class);
+        Cat catOtherBean = applicationContext.getBean(Cat.class);
+
+        System.out.printf("bean==otherBean %b%ncatBean==catOtherBean %b%n",
+                bean.equals(otherBean), catBean.equals(catOtherBean));
     }
 }
